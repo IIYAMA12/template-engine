f6752bcbbe1a8f684ceb2a0b0047d649e710bf25
IIYAMA12 <jorik@207.188.028.145.hva.nl>
2018-04-09 11:23:03 +0200

    add first-version

---
+++ readme.md
@@ -0,0 +1,474 @@
+# Template engine
+
+First of al, this system/syntax really doesn't look like a template engine. Neither it will do just that. It is for you to judge what this suppose to be.
+
+[Source code](https://github.com/IIYAMA12/wafs/blob/master/app/static/scripts/template-engine.js)
+
+Lets get started with what it can do. (basics)
+
+## Basics
+With basics, I mean really basics. Just let JavaScript generate different types of content. (Except for using functions)
+
+
+### Building an element
+Creating a single section. Each object inside of the template should be considered as an instruction.
+* content: This is where you add your content which should matches the `type`.
+* type: There are 4 different types. `tag`, `text`, `html`, `function`. Each of them require different content.
+
+* tag: Datatype TEXT, a tagName.
+* text: Datatype TEXT, any text you want.
+* html: Datatype TEXT, html.
+* function: Datatype FUNCTION, a function.
+
+
+```JS
+[
+    {
+        content: "section",
+        type: "tag",
+    }
+]
+```
+
+Result:
+```HTML
+<section></section>
+```
+
+### Building with sub elements
+If you want to insert sub elements. Then use the keynames `children` or `child` depending on the amount of elements. The keyname `children` stands for the ability to insert multiple child’s. You will need to wrap them around with an array.
+
+```JS
+[
+    {
+        content: "section",
+        type: "tag",
+        children: [
+            {
+                content: "h1",
+                type:"tag"
+            },
+            {
+                content: "p",
+                type: "tag",
+            }
+        ]
+    }
+]
+```
+
+```HTML
+<section>
+    <h1></h1>
+    <p></p>
+</section>
+```
+
+
+### Types in action: `tag`, `text`, `html`
+
+```JS
+[
+    {
+        content: "section",
+        type: "tag",
+        children: [
+            {
+                content: "h1",
+                type:"tag",
+                child: {
+                    content: "Title text",
+                    type: "text"
+                }
+            },
+            {
+                content: "p",
+                type: "tag",
+                child: {
+                    content: "My body text",
+                    type: "text"
+                }
+            }
+        ]
+    },
+    {
+        content: "article",
+        type: "tag",
+        child: {
+            content: "p",
+            type: "tag",
+            child: {
+                content: "Another piece of text",
+                type: "text"
+            }
+        }
+    },
+    {
+        content: "<article><p>Dirty html</p></article>",
+        type: "html"
+    }
+]
+```
+
+Result:
+```HTML
+<section>
+    <h1>Title text</h1>
+    <p>My body text</p>
+</section>
+<article>
+    <p>Another piece of text</p>
+</article>
+<div template-engine-wrapper="true">
+    <article><p>Dirty html</p></article>
+</div>
+```
+
+### Load template
+For loading a template, you have to call the render method of the templateEngine. You have to pass the template into the first argument. The second argument has to be filled in with a DOM element, which where the template starts from applying instructions.
+
+#### Syntax
+```JS
+    templateEngine.render(array template, Note startAtElement [, mix data]);
+```
+Required Arguments
+* `template` A array value which contains the template.
+* `startAtElement` A Note value which contains a dom element, where the engine will start building in.
+
+Optional Arguments
+* `data` A variable of any type, which you want to use in the template.
+
+```JS
+// The template
+const template = [
+    // ...
+];
+
+// Inside this element is where the template engine will start building up.
+const startAtElement = document.getElementById("id");
+
+// Add data to the root. (Note: the data which is flowing inside of the system is the original data. Make a copy of it, if you are going to mutate it inside.)
+const data = {};
+
+templateEngine.render(template, startAtElement, data);
+```
+
+
+I do recommended not to delete the template objects. Because when the exactly same template is applied again, all old elements will be `deleted automatic`. This will be done by cutting down the subRoot elements on the bottom. Which means it will cut the first layer of elements, that are directly in the main array.
+
+
+#### SubRoot
+```JS
+[
+    {
+        content: "section",
+        type: "tag",
+    },
+    {
+        content: "section",
+        type: "tag",
+    },
+    {
+        content: "section",
+        type: "tag",
+    },
+]
+```
+
+Result: (SubRoot elements)
+```HTML
+<section></section>
+<section></section>
+<section></section>
+```
+
+## Advanced
+
+### Query selection support
+
+```HTML
+<section id="test">
+</section>
+```
+
+```JS
+[
+    {
+        query: "#test",
+        content: "p",
+        type: "tag",
+        child: {
+            content: "I have been found!",
+            type:"text"
+        }
+    }
+]
+```
+
+Result:
+```HTML
+<section id="test">
+    <p>I have been found!</p>
+</section>
+```
+
+All queries will be applied to a querySelectorAll function. Keep in mind that the queries do start from the parent.
+Queries that start with `combinators` are also supported:
+
+```CSS
+> p {
+
+}
+
++ article {
+
+}
+
+~ article {
+
+}
+```
+
+#### Limit query results
+The result can be limited with the `limit` property. The value 3 will limit the amount of results to 3.
+* If not correctly defined, it will get all possible elements.
+
+```HTML
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+```
+
+```JS
+[
+    {
+        query: "p",
+        content: "[use-data]",
+        type: "text",
+        data: "Text",
+        limit: 3
+    }
+]
+```
+Result
+```HTML
+<p>Text</p>
+<p>Text</p>
+<p>Text</p>
+<p></p>
+<p></p>
+```
+
+### Functions
+Using functions makes creating content much more easy. This function will be called when the template is rendered.
+
+```JS
+[
+    {
+        content: "ul",
+        type: "tag",
+        child: {
+            content: function (data) {
+                const elements = [];
+                for (let i = 0; i < data.length; i++) {
+                    elements[elements.length] = document.createElement("li");
+                }
+                return elements;
+            },
+            type: "function",
+            data: ["item:1", "item:2", "item:3"]
+        }
+    }
+]
+```
+
+Result:
+```HTML
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+```
+
+
+#### Sending back
+Send multiple elements back.
+
+```JS
+[
+    {
+        content: function () {
+
+            const element = document.createElement("p");
+            const secondElement = document.createElement("p");
+
+            return [{element: element}, {element: secondElement}];
+        },
+        type: "function",
+    }
+]
+```
+Send just one element back.
+```JS
+[
+    {
+        content: function () {
+
+            const element = document.createElement("p");
+
+            return {element: element};
+        },
+        type: "function",
+    }
+]
+```
+
+### Attach data on an instruction
+
+```JS
+[
+    {
+        content: "[use-data]",
+        type: "tag",
+        data: "section"
+    }
+]
+```
+
+Result:
+```HTML
+<section></section>
+```
+
+The keyword `[use-data]` is used by the `text` and `tag` type to use the passed data.
+```JS
+"[use-data]"
+```
+
+
+
+### Passing data to a text child
+If you want to pass data to a child or children, then you have to `create an object` which contains the element as well as the data you want to pass.
+
+```JS
+{element: DOM_element, data: data}
+```
+
+
+```JS
+[
+    {
+        content: "ul",
+        type: "tag",
+        child: {
+            content: function (data) {
+                const elementsWithData = [];
+                for (let i = 0; i < data.length; i++) {
+                    elementsWithData[elementsWithData.length] = {element: document.createElement("li"), data: data[i]};
+                }
+                return elementsWithData;
+            },
+            type: "function",
+            child: {
+                content: "[use-data]",
+                type: "text"
+            }
+        },
+        data: ["item:1", "item:2", "item:3"]
+    }
+]
+```
+
+Result:
+```HTML
+<ul>
+    <li>item:1</li>
+    <li>item:2</li>
+    <li>item:3</li>
+</ul>
+```
+
+
+### Inheritance
+Once data has been send to the first layer of children. The data will be passed automatic to the children of the children.
+
+```JS
+[
+    {
+        content: "ul",
+        type: "tag",
+        child: {
+            content: function () {
+                const elementsWithData = [];
+                for (let i = 0; i < 4; i++) {
+                    elementsWithData[elementsWithData.length] = {element: document.createElement("li"), data: "p"};
+                }
+                return elementsWithData;
+            },
+            type: "function",
+            child: {
+                content: "[use-data]",
+                type: "tag",
+                child: {
+                    content: "[use-data]",
+                    type: "text"
+                }
+            }
+        }
+    }
+]
+```
+
+Result:
+```HTML
+<ul>
+    <li><p>p</p></li>
+    <li><p>p</p></li>
+    <li><p>p</p></li>
+    <li><p>p</p></li>
+</ul>
+```
+
+### Receive data and parent in to a function
+When data is add by a parent, a child with the type `function` can receive the data at it's first parameter.
+
+```JS
+content: function (data, parent) {
+    console.dir(data); // data can be found here!
+    console.dir(parent); // parent can be found here!
+},
+```
+
+```JS
+[
+    {
+        content: "ul",
+        type: "tag",
+        child: {
+            content: function (_, parent) {
+
+                console.dir(parent); // <ul> can be found here!
+
+                const elementsWithData = [];
+                for (let i = 0; i < 4; i++) {
+                    elementsWithData[elementsWithData.length] = {element: document.createElement("li"), data: "p"};
+                }
+                return elementsWithData;
+            },
+            type: "function",
+            child: {
+                content: function (data, parent) {
+                    console.dir(data); // "p" can be found here!
+                    console.dir(parent); // <li> can be found here!
+                },
+                type: "function",
+            }
+        }
+    }
+]
+```
+
+[Source code](https://github.com/IIYAMA12/wafs/blob/master/app/static/scripts/template-engine.js)
---
+++ template-engine.js
@@ -0,0 +1,268 @@
+const templateEngine = (function () {
+    const templateEngine = {
+        render: function (template, parentElement, data) {
+            if (template != undefined && template.length > 0) {
+
+                // Two new fragments. It requires two of them to use connectors directly on elements.
+
+
+
+
+                const apply = this.apply; // << optimisation
+
+                // Execute the apply function for every instruction
+                for (let i = 0; i < template.length; i++) {
+
+
+                    template[i].subRoot = true; // this is for cutting the dom tree only at the bottom.
+
+                    apply(template[i], parentElement, data, i);
+
+                    /* - - - - - - - - - - - - - - - - - - - - - */
+                    /* experimental document fragment */
+                    // First attempt has been a failure
+                    /* - - - - - - - - - - - - - - - - - - - - - */
+                    // const parentFragment = document.createDocumentFragment();
+                    // const firstTempContainer = document.createElement("div");
+                    // const secondTempContainer = document.createElement("div");
+                    //
+                    // parentFragment.appendChild(firstTempContainer);
+                    // firstTempContainer.appendChild(secondTempContainer);
+
+                    // const subRootChildren = secondTempContainer.children;
+                    // for (let i = 0; i < subRootChildren.length; i++) {
+                    //     parentFragment.appendChild(subRootChildren[i]);
+                    // }
+                    // parentFragment.removeChild(firstTempContainer);
+                    //
+                    // // append the fragment on to the parent
+                    // parentElement.appendChild(parentFragment);
+                    /* - - - - - - - - - - - - - - - - - - - - - */
+                }
+            }
+        },
+        addContent: function (parent, type, content, data) {
+
+            if (parent != undefined && content != undefined) {
+                if (type == undefined) { // no type? Use html for debug purposes.
+                    type = "html";
+                }
+
+                switch(type) {
+                    case "function": // Let a custom function handle how the elements are created.
+                        return (function () {
+                            let elementsOrDataWithElement = content(data, parent);
+
+
+                            if (typeof(elementsOrDataWithElement) == "object") {
+
+                                // If it is just a single element, wrap it with an array.
+                                if (!Array.isArray(elementsOrDataWithElement)) {
+                                    elementsOrDataWithElement = [elementsOrDataWithElement];
+                                }
+
+                                if (elementsOrDataWithElement.length > 0) {
+                                    const elementsWithData = [];
+                                    const isElement = app.utility.isElement;
+                                    for (let i = 0; i < elementsOrDataWithElement.length; i++) {
+                                        const elementOrDataWithElement = elementsOrDataWithElement[i];
+                                        if (isElement(elementOrDataWithElement)) {
+                                            parent.append(elementOrDataWithElement);
+                                            elementsWithData[elementsWithData.length] = {element: elementOrDataWithElement, data: data};
+
+                                        } else if (typeof(elementsOrDataWithElement) == "object") {
+                                            if (elementOrDataWithElement.element !== undefined) { // if DEFINED
+                                                if (isElement(elementOrDataWithElement.element)) {
+                                                    elementsWithData[elementsWithData.length] = elementOrDataWithElement;
+                                                    parent.append(elementOrDataWithElement.element);
+                                                }
+                                            } else { // Just only for data
+
+                                                elementOrDataWithElement.element = parent;
+                                                elementsWithData[elementsWithData.length] = elementOrDataWithElement;
+                                            }
+                                        }
+                                    }
+                                    return elementsWithData;
+                                }
+                            }
+                            return false;
+                        })();
+
+                    case "html": // html injection
+                        return (function () {
+                            // A wrapper is required to make sure that injected html code is removed. I do not prefer to use this `case`. But might be handy if it comes to that.
+                            const newElement = document.createTextNode("div");
+                            newElement.innerHTML += content;
+                            newElement.setAttribute("template-engine-wrapper", true);
+                            parent.append(newElement);
+                            return [{element: newElement, data: data}];
+                        })();
+
+                    case "text": // create textContent
+                        return (function () {
+
+                            if (content === "[use-data]"){
+                                content = data;
+                            }
+                            if (content != undefined) {
+                                const newElement = document.createTextNode(content);
+                                parent.textContent = "";
+                                parent.append(newElement);
+                                return true;
+                            }
+                        })();
+
+                    case "tag": // create tag
+                        return (function () {
+                            if (content === "[use-data]"){
+                                content = data;
+                            }
+                            if (content != undefined) {
+                                const newElement = document.createElement(content);
+                                parent.append(newElement);
+                                return [{element: newElement, data: data}];
+                            }
+                        })();
+                }
+            }
+        },
+        apply: function (instruction, parent, data) {
+            let query = "";
+
+            // Set the parent as default start point.
+            let parentElements = [parent];
+
+            // Check and prepare query
+            let newQuery = instruction.query;
+            if (newQuery != undefined) {
+                newQuery = newQuery.trim();
+                if (newQuery != "") {
+                    query = newQuery;
+                }
+            }
+
+            // we found (new) data, lets flow it in the system.
+            if (instruction.data != undefined) {
+                data = instruction.data;
+            }
+
+
+            if (instruction) {
+                let content = instruction.content;
+                if (content != undefined && parent != undefined) {
+                    if (query != undefined && query != "") {
+                        const previousParent = parent.parentElement;
+                        if (previousParent != undefined) {
+
+                            // Using a tempolary attribute to use for the query selector. Which is used to support selections like this: `> tagName`. It is a hack!
+                            parent.setAttribute("template-enige-temp", true);
+
+                            if (instruction.limit == undefined || instruction.limit > 1) {
+                                parentElements = previousParent.querySelectorAll("[template-enige-temp] " + query);
+                                if (parentElements != undefined && parentElements.length > instruction.limit) {
+                                    parentElements = Array.from(parentElements).slice(0, instruction.limit);
+                                }
+                            } else {
+                                const selection = previousParent.querySelector("[template-enige-temp] " + query);
+                                if (selection != undefined) {
+                                    parentElements = [selection];
+                                } else {
+                                    parentElements = null;
+                                }
+                            }
+
+                            parent.removeAttribute("template-enige-temp");
+
+                        } else {
+                            console.error("No parent found for template engine. Section: querySelector. Query:", query);
+                        }
+                    }
+                    if (parentElements != undefined && parentElements.length > 0) {
+
+                        // experimental methods to no replace. Shouldn't be used at the moment because it might cause some unexpected results. Use the `function` type in template instead.
+                        let method = instruction.method;
+                        if (method == undefined) {
+                            method = "replace";
+                        }
+
+                        if (method == "replace") {
+
+                            // Get all old elements.
+                            let oldElements = instruction.elements;
+
+                            if (oldElements != undefined) {
+
+                                // are the oldElements and are they are the subRoot. (Which is the first layer of elements)
+                                if (instruction.subRoot && oldElements.length > 0) {
+
+                                    const isElement = app.utility.isElement; // get utility element validator
+
+                                    // Remove old subRoot elements.
+                                    for (let i = 0; i < oldElements.length; i++) {
+                                        const oldElement = oldElements[i];
+                                        if (isElement(oldElement)) {
+                                            oldElement.parentNode.removeChild(oldElement);
+                                        }
+                                    }
+                                }
+
+                                // Delete the references just to be clean.
+                                delete instruction.elements;
+                            }
+                        }
+
+                        // get all children from the instruction
+                        let instructionChildren = instruction.children;
+
+                        // add .child support
+                        if (instructionChildren == undefined && instruction.child != undefined) {
+                            instructionChildren = [instruction.child];
+                        }
+
+                        // collection of all elements. The bottom layer of the element will be used to cut off.
+                        instruction.elements = [];
+
+                        const apply = templateEngine.apply; // << optimisation
+
+                        // these are the elements from the query all selection
+                        for (let i = 0; i < parentElements.length; i++) {
+                            const parentElement = parentElements[i];
+                            // Make the new elements.
+                            const newElementsWithData = templateEngine.addContent(parentElement, instruction.type, content, data);
+
+                            // Validate the outcome.
+                            if (newElementsWithData != undefined && newElementsWithData !== true && newElementsWithData.length > 0) {
+
+                                // Go through all new created elements.
+                                for (let elementIndex = 0; elementIndex < newElementsWithData.length; elementIndex++) {
+
+
+                                    const newElementWithData = newElementsWithData[elementIndex];
+                                    if (newElementWithData != undefined) {
+                                        const newElement = newElementWithData.element;
+                                        // collect the new created elements
+                                        instruction.elements[instruction.elements.length] = newElement;
+
+                                        // apply all new instructions
+                                        if (instructionChildren != undefined && instructionChildren.length > 0) {
+
+                                            // All new instructions apply to new elements.
+                                            for (let j = 0; j < instructionChildren.length; j++) {
+                                                apply(instructionChildren[j], newElement, newElementWithData.data);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    };
+
+    return templateEngine;
+})();
